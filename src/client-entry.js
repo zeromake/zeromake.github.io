b367fc8aa1b572a0047c3aa1dbca366e515f19dc
zeromake <a390720046@gmail.com>
2017-06-22 16:10:48 +0800

    fix: route hash lost

--- src/client-entry.js
+++ src/client-entry.js
@@ -28,8 +28,8 @@ const { app, router, store } = createApp()
 
 if (window.__INITIAL_STATE__) {
     let url = location.pathname
-    if (location.search) url += '?' + location.search
-    if (location.hash) url += '#' + location.search
+    if (location.search) url += location.search
+    if (location.hash) url += location.hash
     const nowRoute = router.match(url)
     window.__INITIAL_STATE__.route.query = nowRoute.query
     window.__INITIAL_STATE__.route.hash = nowRoute.hash
