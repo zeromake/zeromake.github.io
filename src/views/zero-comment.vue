e917e04c1a30e57bca658649015108a967aa9b59
zeromake <a390720046@gmail.com>
2019-05-06 09:28:52 +0800

    fix: gitment -> gitalk

--- src/views/zero-comment.vue
+++ src/views/zero-comment.vue
@@ -19,13 +19,18 @@ export default {
             const page = this.param === 'path' ? this.$route.path.substr(1) : this.$route.params[this.param];
             if (this.$gitment) {
                 const gitment = new this.$gitment({
+                    // container: page,
                     id: page, // 可选。默认为 location.href
                     owner: 'zeromake',
                     repo: 'zeromake.github.io',
-                    oauth: {
-                        'client_id': '6f4e103c0af2b0629e01',
-                        'client_secret': '22f0c21510acbdda03c9067ee3aa2aee0c805c9f'
-                    }
+                    clientID: '6f4e103c0af2b0629e01',
+                    clientSecret: '22f0c21510acbdda03c9067ee3aa2aee0c805c9f',
+                    admin: ['zeromake'],
+                    labels: ['gitment']
+                    // oauth: {
+                    //     'client_id': '6f4e103c0af2b0629e01',
+                    //     'client_secret': '22f0c21510acbdda03c9067ee3aa2aee0c805c9f'
+                    // }
                 });
                 gitment.render(thread);
                 this.$store.commit('COMMENT_CONTANIER', { select: '.comment-contanier' });
