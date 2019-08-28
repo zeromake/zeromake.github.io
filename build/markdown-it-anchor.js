07b2fd368df7377afd1eac8e3c5aaf6d23df005e
zeromake <a390720046@gmail.com>
2019-08-28 18:12:56 +0800

    feat: add markdon-it anchor plugin

--- build/markdown-it-anchor.js
+++ build/markdown-it-anchor.js
@@ -1,11 +1,109 @@
+const util = require("hexo-util");
+
+const hasProp = Object.prototype.hasOwnProperty;
+
+function anchorId(str, transformOption) {
+    return util.slugize(str.trim(), { transform: transformOption });
+}
+
+function uniqueSlug(slug, slugs) {
+    let uniq = slug
+    let i = 2
+    while (hasProp.call(slugs, uniq)) uniq = `${slug}-${i++}`
+    slugs[uniq] = true
+    return uniq
+}
+
+/**
+ *
+ * @param {import('markdown-it').RulerBlock} ruler
+ */
+function findRule(ruler, name) {
+    for(const rule of ruler.__rules__) {
+        if (name === rule.name) {
+            return rule;
+        }
+    }
+}
+
+function slugify(title) {
+    return encodeURIComponent(anchorId(title))
+}
+
+const defaultOption = {
+    after: false,
+    class: 'anchor',
+    slugify,
+    inject: false,
+    content: '¶',
+};
+
 /**
  * @param {import('markdown-it')} md
  */
 module.exports = function (md, option) {
-    // md.block.ruler.after('heading', 'anchor', function(state, start, end, silent) {
-    //     const last = state.tokens[state.tokens.length - 1];
-    //     if(last.type === 'heading_close') {
-    //         console.log(last);
-    //     }
-    // });
+    option = Object.assign({}, defaultOption, option);
+    function anchor(state) {
+        const len = state.tokens.length;
+        let index = len - 2;
+        let token = state.tokens[index];
+        const content = [];
+        while (token && token.type !== 'heading_open') {
+            content.push(token.content);
+            index--;
+            token = state.tokens[index];
+        }
+        const title = content.reverse().join('');
+        const env = state.env;
+        const slugs = env.slugs || (env.slugs = {});
+        const tocs = env.tocs;
+        let id = token.attrGet('id');
+        if(!id) {
+            id = option.slugify(title);
+        } else {
+            if(option.inject) {
+                token.attrSet('id', null);
+            }
+        }
+        id = uniqueSlug(id, slugs);
+        const level = token.markup.length;
+        if(tocs) {
+            tocs.push({
+                id,
+                title,
+                level,
+            });
+        }
+        const injectIndex = option.after ? len - 1 : index + 1;
+        const linkStart = new state.Token('link_open', 'a', 1);
+        linkStart.attrs = [
+            ['class', option.class],
+            ['href', `#${id}`],
+        ];
+        if(option.inject) {
+            linkStart.attrs.push(['id', id]);
+        }
+        const linkClose = new state.Token('link_close', 'a', -1);
+        if(option.content) {
+            const text = new state.Token('html_block', '', 0);
+            text.content = option.content;
+            state.tokens.splice(injectIndex, 0, linkStart, text, linkClose);
+        } else {
+            state.tokens.splice(injectIndex, 0, linkStart, linkClose);
+        }
+    }
+
+    const heading = findRule(md.block.ruler, 'heading');
+    if(!heading) {
+        throw new Error('Parser rule not found: heading');
+    }
+    // 取出方法引用，防止出现无限递归
+    const headingFn = heading.fn;
+    md.block.ruler.at('heading', function headingAnchor(state, startLine, endLine, silent) {
+        const flag = headingFn(state, startLine, endLine, silent);
+        if(flag) {
+            anchor(state);
+        }
+        return flag;
+    }, { alt: heading.alt });
 };
--- build/markdown-it-renderer.js
+++ build/markdown-it-renderer.js
@@ -3,19 +3,9 @@ const katex = require('@iktakahiro/markdown-it-katex');
 const highlight = require('./highlight');
 const lineNumbers = require('./highlight-line-numbers');
 const anchor = require('./markdown-it-anchor');
-
-const util = require("hexo-util");
-const stripHTML = util.stripHTML;
-
-
-function anchorId(str, transformOption) {
-    return util.slugize(str.trim(), { transform: transformOption });
-}
-
 const langPrefix = 'language-';
 
 module.exports = () => {
-    let tocs = [];
     const md = markdownIt('commonmark', {
         langPrefix,
         highlight(code, lang) {
@@ -50,40 +40,21 @@ module.exports = () => {
     });
 
     md.enable(['table']);
-    md.use(anchor);
+    md.use(anchor, {
+        class: 'headerlink anchor',
+        after: true,
+        inject: true,
+    });
     md.use(katex);
-    md.renderer.rules.heading_close = (tokens, idx) => {
-        const token = tokens[idx];
-        const content = [];
-        let index = idx - 1;
-        let t = tokens[index];
-        while(t && t.type !== 'heading_open') {
-            content.push(t.children.reduce((c, i) => {
-                if(i.type === 'text' || i.type === 'code_inline') {
-                    c += i.content;
-                }
-                return c;
-            }, ''));
-            index--;
-            t = tokens[index];
-        }
-        const title = stripHTML(content.reverse().join(''));
-        const id = encodeURIComponent(anchorId(title));
-        const level = token.markup.trim().length;
-        tocs.push({
-            id,
-            title,
-            level,
-        });
-        return `<a id="${id}" href="#${id}" class="headerlink anchor"></a></${token.tag}>`;
-    };
     return {
         render(text) {
             return md.render(text);
         },
         renderToc(text) {
-            tocs = [];
-            return [md.render(text), tocs];
+            const env = {
+                tocs: [],
+            };
+            return [md.render(text, env), env.tocs];
         }
     }
 }
